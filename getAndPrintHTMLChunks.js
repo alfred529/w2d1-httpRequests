957841dd5912c7c441436886a009af4c39cf5114
Alfred Law <alfred.lawsn@gmail.com>
2018-09-03 15:25:36 +0000

    finished step 1 - printing in chunks

---
+++ getAndPrintHTMLChunks.js
@@ -0,0 +1,34 @@
+// Your first function getAndPrintHTMLChunks will utilize the https library to
+// GET a given URL.
+// You can use the example above as a reference or template.
+// This function should console.log each chunk of data as it is received, concatenated
+// with a newline character ('\n') so you can visualize each chunk.
+
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log(data + '\n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+getAndPrintHTMLChunks()
